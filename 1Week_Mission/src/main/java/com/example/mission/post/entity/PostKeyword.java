f591fbc3fdd20aff483b8ea78f4ff3838b1ce8fd
jihun-24k <gi020477@gmail.com>
2022-10-17 15:24:43 +0900

    Feat: 엔티티 추가

---
+++ 1Week_Mission/src/main/java/com/example/mission/post/entity/PostKeyword.java
@@ -0,0 +1,21 @@
+package com.example.mission.post.entity;
+
+import javax.persistence.Entity;
+
+import com.example.mission.base.entity.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class PostKeyword extends BaseEntity {
+}
