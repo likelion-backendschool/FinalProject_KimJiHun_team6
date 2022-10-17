79022fa9761603420582e53df88641f240af5ed8
jihun-24k <gi020477@gmail.com>
2022-10-17 16:40:03 +0900

    Test: 회원가입폼 Get url 테스트 코드 추가

---
+++ 1Week_Mission/src/test/java/com/example/mission/member/controller/MemberControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.mission.member.controller;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class MemberControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	@DisplayName("회원가입폼 Get url 테스트")
+	void t1() throws Exception {
+		ResultActions resultActions = mvc
+			.perform(get("member/join"))
+			.andDo(print());
+
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(handler().handlerType(MemberController.class))
+			.andExpect(handler().methodName("memberJoin"));
+
+	}
+}
