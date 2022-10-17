341c00f57e96d118c06904bb779c24510b666bf8
jihun-24k <gi020477@gmail.com>
2022-10-17 11:11:56 +0900

    Chore: 1주차 프로젝트 세팅

---
+++ 1Week_Mission/src/test/java/com/example/Week_Mission/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Week_Mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
