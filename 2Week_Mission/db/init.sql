04ae608b911d242cea321bc7ac338896d22170c2
jihun-24k <gi020477@gmail.com>
2022-10-26 11:34:57 +0900

    Chore: DDL init 파일 수정

--- 2Week_Mission/db/init.sql
+++ 2Week_Mission/db/init.sql
@@ -1,9 +1,9 @@
 # 테스트 DB 생성
-DROP DATABASE IF EXISTS final__2022_10_08__test;
-CREATE DATABASE final__2022_10_08__test;
-USE final__2022_10_08__test;
+DROP DATABASE IF EXISTS mutbooks_test;
+CREATE DATABASE mutbooks_test;
+USE mutbooks_test;
 
 # 개발 DB 생성
-DROP DATABASE IF EXISTS final__2022_10_08__dev;
-CREATE DATABASE final__2022_10_08__dev;
-USE final__2022_10_08__dev;
+DROP DATABASE IF EXISTS mutbooks_dev;
+CREATE DATABASE mutbooks_dev;
+USE mutbooks_dev;
