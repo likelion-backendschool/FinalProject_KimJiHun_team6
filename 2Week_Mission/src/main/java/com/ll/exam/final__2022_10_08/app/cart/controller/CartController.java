08e5d1c324239629bff8884b687ff72b0dad3d0b
jihun-24k <gi020477@gmail.com>
2022-10-26 09:29:08 +0900

    Feat: 장바구니에 상품추가 기능 구현

--- 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/cart/controller/CartController.java
+++ 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/cart/controller/CartController.java
@@ -1,10 +1,13 @@
 package com.ll.exam.final__2022_10_08.app.cart.controller;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,12 +18,16 @@ import com.ll.exam.final__2022_10_08.app.base.rq.Rq;
 import com.ll.exam.final__2022_10_08.app.cart.entity.CartItem;
 import com.ll.exam.final__2022_10_08.app.cart.service.CartService;
 import com.ll.exam.final__2022_10_08.app.member.entity.Member;
+import com.ll.exam.final__2022_10_08.app.product.entity.Product;
+import com.ll.exam.final__2022_10_08.app.product.service.ProductService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/cart")
+@Slf4j
 public class CartController {
 	private final CartService cartService;
+	private final ProductService productService;
 	private final Rq rq;
 
 	@GetMapping("/list")
@@ -37,7 +44,12 @@ public class CartController {
 
 	@PostMapping("/add/{id}")
 	@PreAuthorize("isAuthenticated()")
-	public String addItem(){
+	public String addItem(@PathVariable long id){
+		Member buyer = rq.getMember();
+		Product product = productService.findById(id).get();
+		CartItem cartItem = cartService.addItem(buyer, product);
+		log.info("Int!!!! Here: "+String.valueOf(id));
+		log.info("Hello!!!! Here: "+String.valueOf(cartItem));
 		return Rq.redirectWithMsg("/product/list","상품이 추가됐습니다.");
 	}
 
@@ -57,6 +69,6 @@ public class CartController {
 					cartService.removeItem(cartItem);
 				}
 			});
-		return Rq.redirectWithMsg("/cart/items", "%d건의 품목을 삭제하였습니다.".formatted(idsArr.length));
+		return Rq.redirectWithMsg("/cart/list", "%d건의 품목을 삭제하였습니다.".formatted(idsArr.length));
 	}
 }
